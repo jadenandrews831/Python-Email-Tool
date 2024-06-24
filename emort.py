6a2b5b0828acbf50b8d9fb754e705de5de89cb48
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2024-06-24 15:10:07 -0400

    client can now send emails

--- emort.py
+++ emort.py
@@ -1,6 +1,10 @@
 from cryptography.fernet import Fernet
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from io import StringIO
 
 import argparse
+import csv
 import getpass
 import json
 import os
@@ -11,6 +15,8 @@ import subprocess
 import sys
 import textwrap
 import threading
+
+config_path = 'dist/config/config.json'
 
 def main():
   parser = argparse.ArgumentParser(
@@ -45,7 +51,7 @@ def main():
     creds_prompt()
   
 def check_config():
-  if os.path.exists('dist/config/config.json'):
+  if os.path.exists(config_path):
     return True
   else:
     return False
@@ -67,11 +73,6 @@ def creds_prompt(usr=None, pswrd=None):
       config = write_config(usr, pswrd, smtp, port)
       email_signin(config)
 
-def make_group(name, members):
-  config = decrypt_config()
-  print("Config:\n", config)
-    
-
 def email_signin(config):
   usr = config["username"]
   pswrd = config["password"]
@@ -82,13 +83,61 @@ def email_signin(config):
 
   context = ssl.create_default_context()
 
-  with smtplib.SMTP_SSL(SMTP, port, context=context) as server:
-    server.login(usr, pswrd)
-    print("Logged in to mail server", SMTP)
-    return True
+  server = smtplib.SMTP_SSL(SMTP, port, context=context)
+  server.login(usr, pswrd)
+  print("Logged in to mail server", SMTP)
+  return True, server
   
   return False
     # server.sendmail(usr, None, )
+
+def make_group(config, file, g_name):
+  if hasattr(config, 'groups'):
+    pass
+  else:
+    print("Making Group...")
+    print('config: ', config)
+    if not 'groups' in config.keys():
+      config['groups'] = {}
+    config['groups'][g_name] = []
+    print('Updated Config:', config)
+    ext = file.split('.')[-1]
+    print("\nExtension:", ext)
+
+    if ext == 'json':
+      with open(file, 'rb') as f:
+        data = f.read().decode()
+        print("File Data:", type(data))
+        obj = json.loads(data)
+        print("obj:", obj)
+        for (key, val) in obj.items():
+          config['groups'][g_name].append(val)
+
+        print('Config:', config)
+      with open(config_path, 'wb') as f:
+        f.write(json.dumps(config).encode())
+
+      encrypt_config(config_path)
+
+    elif ext == 'csv':
+      with open(file, 'rb') as f:
+        data = f.read().decode('utf-8', errors='replace')
+        print('File Data:', data)
+        fieldnames = ("email","full_name","phone_number")
+        csvReader = list(csv.DictReader(StringIO(data)))
+        print('csvReader: ', csvReader)
+        for row in csvReader:
+          print("Row:", row)
+          if 'full_name' in row.keys():
+            row["f_name"] = row['full_name'].split()[0]
+            row["l_name"] = row['full_name'].split()[1]
+          print("New Row:", row)
+          config['groups'][g_name].append(row)
+        with open(config_path, 'wb') as f:
+          f.write(json.dumps(config).encode())
+
+      encrypt_config(config_path)
+
 
 
 def write_config(usr, pswrd, smtp, port):
@@ -166,8 +215,23 @@ def decrypt_config():
     print("Unable to decrypt. Please Try Again later", e)
     sys.exit(1)
 
-def send_email():
-  pass
+def send_email(config, gname, email, server):
+  print(f'[+ emort] Email:\n{email}\n')
+  print(f'[+ emort] Server: {server}\n')
+  print(f'[+ emort] Config: {config}')
+
+  group = config['groups'][gname]
+  print(f'[+ emort] Group: {group}')
+  server.set_debuglevel(1)
+  for usr in group:
+    msg = MIMEMultipart()
+    msg['From'] = config['username']
+    msg['To'] = usr['email']
+    msg['Subject'] = 'Test Email'   #Fix Me: Receive a subjet form the GUI
+    msg.attach(MIMEText(email, 'plain'))
+
+    print(f'\n\n[+ emort][send_email] Email: {usr['email']}\n')
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
 
 if __name__ == "__main__":
   main()
--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import emort
 import tkinter as tk
 
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+
 root = tk.Tk()
 
 root.title("Email Marketing Tool")
@@ -12,12 +15,15 @@ smtp_var = tk.StringVar()
 port_var = tk.StringVar()
 text_var = tk.StringVar()
 foi_var = tk.StringVar()
+fp_var = tk.StringVar()
+gn_var = tk.StringVar()
+em_var = tk.StringVar()
 
 def reset_display():
   for child in root.winfo_children():
     child.destroy()
 
-def emort_signin():
+def emort_signin(config):
   reset_display()
 
   usr = name_var.get()
@@ -28,18 +34,20 @@ def emort_signin():
   print(f"INFO >>>\n\n{usr}\n{pss}\n{smtp}\n{port}")
   data = emort.write_config(usr, pss, smtp, port)
   print(f"Data >>>\n{data}")
-  gui_main()
+  gui_main(config)
 
-def emort_sendemail(email):
+def emort_sendemail(email, config, gname, server):
+  print("Email:\n\n ", email)
+  emort.send_email(config, gname, email, server)
+  gui_main(config, server)
+
+def gui_main(config, server=None):
   reset_display()
 
-  print("Email:\n\n ", email)
-
-def gui_main(config):
   name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
   server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
   groups_label = tk.Label(root, text="Groups:", font=('calibre', 20, 'bold'))
-  ag_btn = tk.Button(root, text="+ Group", command=lambda: gui_makegroup(config))
+  ag_btn = tk.Button(root, text="+ Group", command=lambda: gui_makegroup(config, server))
 
   name_label.grid(column=0, row=0)
   server_label.grid(column=0, row=1)
@@ -47,34 +55,27 @@ def gui_main(config):
   ag_btn.grid(column=1, row=3)
 
   print("Config:", config)
-  if hasattr(config, 'groups'):
+  print("Config Keys:", config.keys())
+  if 'groups' in config.keys():
     names = config['groups'].keys()
+    row = 4
     for name in names:
-      print("Name")
+      tk.Label(root, text=name+'\n'+str(config['groups'][name][0]), font=('calibre', 10, 'bold')).grid(column=0, row=row)
+      tk.Button(root, text="Email Group", command=lambda: gui_makemail(config, name, server)).grid(column=1, row=row)
+      row += 1
   else:
     ng_label = tk.Label(root, text="No Groups Available", font=('calibre', 18))
     ng_label.grid(column=0, rows=4)
 
- 
-
   root.mainloop()
 
-def gui_grpoptions(config):
+def gui_makegroup(config, server):
   reset_display()
 
-  name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
-  server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
-  ag_label = tk.Label(root, text="Add Group:", font=('calibre', 20, 'bold'))
+  val = foi_var.get()
+  print("VAL:", val)
 
-  name_label.grid(column=0, row=0)
-  server_label.grid(column=0, row=1)
-  ag_label.grid(column=0, row=3)
-
-  vals = {"File": 1, "Input": 2}
-  
-
-def gui_makegroup(config):
-  reset_display()
+  v = val
 
   name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
   server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
@@ -86,17 +87,71 @@ def gui_makegroup(config):
 
   vals = {"File": 1, "Input": 2}
   for (name, val) in vals.items():
-    tk.Radiobutton(root, text = name, variable = foi_var, value = val, command=lambda: gui_grpoptions(config)).grid(column=val, row=3) 
+    tk.Radiobutton(root, text = name, variable = foi_var, value = val, command=lambda: gui_makegroup(config, server)).grid(column=val, row=3) 
 
-  
+  if v == '1':
+    gn_label = tk.Label(root, text="Group Name", font=('calibre', 10, 'bold'))
+    gn_entry = tk.Entry(root, textvariable=gn_var, font=("calibre", 10, 'bold'))
+    fp_label = tk.Label(root, text="File Path", font=('calibre', 10, 'bold'))
+    fp_entry = tk.Entry(root, textvariable=fp_var, font=('calibre', 10, 'bold'))
+    fp_btn = tk.Button(root, text="Browse...", command=lambda: writepath(fp_entry))
+    submit_btn = tk.Button(root, text="Submit", command=lambda: makegroup(config, server))
+
+    gn_label.grid(column=0, row=4)
+    gn_entry.grid(column=1, row=4)
+    fp_label.grid(column=0, row=5)
+    fp_entry.grid(column=1, row=5)
+    fp_btn.grid(column=2, row=5)
+    submit_btn.grid(column=1, row=6)
+
+  elif v == "2":
+    print("Val is 2")
+  else:
+    print("pass")
 
   root.mainloop()
 
+def gui_makemail(config, gname, server):
+  reset_display()
+
+  name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
+  server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
+  em_label = tk.Label(root, text="Email:", font=('Calibre', 20, 'bold'))
+
+  name_label.grid(column=0, row=0)
+  server_label.grid(column=0, row=1)  
+  em_label.grid(column=0, row=2)
+
+  text = tk.Text(root, font=("calibre", 15, 'bold'))
+  text.grid(column=0, row = 3)
+
+  em_btn = tk.Button(root, text="Submit", command=lambda: emort_sendemail(text.get("1.0", "end-1c"), config, gname, server))
+  em_btn.grid(column=0, row=4)
+
+  root.mainloop()
+
+def makegroup(config, server):
+  file_path = fp_var.get()
+  g_name = gn_var.get()
+  print("g_name:", g_name)
+  if g_name == '':
+    g_name = "New Group"  # Make Variable to prevent Overwriting
+  print("File:",file_path)
+  emort.make_group(config, file_path, g_name)
+  gui_main(config, server)
+
+def writepath(fg_entry):
+  file_path = filedialog.askopenfilename(title="Select a Text File", filetypes=[("CSV files", "*.csv"),("JSON files", "*.json")])
+  while True:
+    if file_path:
+      print("File Path:", file_path)
+      fp_var.set(file_path)
+      break
 
 def gui_choose_recp():
   pass
 
-def gui_signin():
+def gui_signin(config=None):
   name_label = tk.Label(root, text="Username", font=('calibre', 10, 'bold'))
   name_entry = tk.Entry(root, textvariable=name_var, font=('calibre', 10, 'bold'))
   passw_label = tk.Label(root, text='Password', font=('calibre', 10, 'bold'))
@@ -105,7 +160,7 @@ def gui_signin():
   smtp_entry = tk.Entry(root, textvariable=smtp_var, font=('calibre', 10, 'bold'))
   port_label = tk.Label(root, text="Port Number (Default 456)", font=('calibre', 10, 'bold'))
   port_entry = tk.Entry(root, textvariable=port_var, font=('calibre', 10, 'bold'))
-  sub_btn = tk.Button(root, text="Submit", command=emort_sendemail)
+  sub_btn = tk.Button(root, text="Submit", command=lambda: emort_signin(config))
 
   name_label.grid(row=0,column=1)
   name_entry.grid(row=0,column=2)
@@ -119,31 +174,31 @@ def gui_signin():
 
   root.mainloop()
 
-def gui_sendemail(config):
-  print("Config:", config)
-  name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
-  server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
-  email_label = tk.Label(root, text="Email Section:", font=('calibre', 20, 'bold'))
-  text_area = tk.Text(root, wrap=tk.WORD, width=100, height=30, font=('Times New Roman', 20))
-  sub_btn = tk.Button(root, text="Submit", command=lambda: emort_sendemail(text_area.get("1.0", "end-1c")))
+# def gui_sendemail(config):
+#   print("Config:", config)
+#   name_label = tk.Label(root, text=config['username'], font=('calibre', 10, 'bold'))
+#   server_label = tk.Label(root, text=config['SMTP'], font=('calibre', 10, 'bold'))
+#   email_label = tk.Label(root, text="Email Section:", font=('calibre', 20, 'bold'))
+#   text_area = tk.Text(root, wrap=tk.WORD, width=100, height=30, font=('Times New Roman', 20))
+#   sub_btn = tk.Button(root, text="Submit", command=lambda: emort_sendemail(text_area.get("1.0", "end-1c")))
 
-  name_label.grid(column=0, row=0)
-  server_label.grid(column=0, row=1)
-  email_label.grid(column=0, row=3)
-  text_area.grid(column=0, row=4, pady=10, padx=10)
-  sub_btn.grid(column=0, row=5)
+#   name_label.grid(column=0, row=0)
+#   server_label.grid(column=0, row=1)
+#   email_label.grid(column=0, row=3)
+#   text_area.grid(column=0, row=4, pady=10, padx=10)
+#   sub_btn.grid(column=0, row=5)
 
-  text_area.focus()
-  root.mainloop()
+#   text_area.focus()
+#   root.mainloop()
 
 def main():
   exists = emort.check_config()
 
   if exists:
     config = emort.decrypt_config()
-    signedin = emort.email_signin(config)
+    signedin, server = emort.email_signin(config)
     if signedin:
-      gui_main(config)
+      gui_main(config, server)
     else:
       gui_signin()
   else:
